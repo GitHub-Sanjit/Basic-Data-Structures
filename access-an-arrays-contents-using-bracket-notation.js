48f8abdb44dcdddcf39ae9a8a5ee3fdf71ba77c2
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-09-12 22:59:33 +0600

    access-an-arrays-contents-using-bracket-notation

---
+++ access-an-arrays-contents-using-bracket-notation.js
@@ -0,0 +1,8 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[0]
+myArray[1] = "j";
+myArray[2]
+myArray[3]
+// Only change code above this line
+console.log(myArray);
